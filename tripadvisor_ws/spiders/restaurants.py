98176d61393c93304da8d2bb3252712c9bfe6634
unknown <jchong0618@gmail.com>
2019-04-07 17:23:54 +0800

    completed restaurants scrapper

--- tripadvisor_ws/spiders/restaurants.py
+++ tripadvisor_ws/spiders/restaurants.py
@@ -22,39 +22,59 @@ class RestaurantsSpider(scrapy.Spider):
     def parse(self, response):
         list = response.css('div#EATERY_SEARCH_RESULTS')
         for el in list.css('div.listing'):
-            href = el.css('div.title').xpath('a/@href').extract_first()
-            yield scrapy.Request('{}{}'.format(self.domain, href), callback=self.parse_restaurant)
+            href = el.css('div.photo_booking').xpath('a/@href').extract_first()
+            if href is None:
+                continue
+            yield scrapy.Request('{}/{}'.format(self.domain, href), callback=self.parse_restaurant)
             # break
 
     def parse_restaurant(self, response):
         restaurant = Restaurant()
-        header = response.css('div#taplc_location_detail_header_restaurants_0')
-        restaurant['name'] = header.css('h1#HEADING').xpath('text()').extract_first()
-        restaurant['phone'] = header.css('div.blEntry.phone').xpath('span/text()').extract_first()
+        header = response.css('div#taplc_resp_rr_top_info_rr_resp_0')
+        restaurant['name'] = header.css('div.restaurantName > h1').xpath('text()').extract_first()
 
-        small_details = response.css('div#taplc_restaurants_detail_info_content_0')
-        cusines = small_details.css('div.cuisines').css('div.text').xpath('text()').extract_first()
-        if cusines != None:
-            restaurant['cusines'] = cusines.strip().split(', ')
-        restaurant['address'] = small_details.css('div.address').css('span.street-address').xpath('text()').extract_first()
+        descriptions = header.css('div.restaurantDescription')
+        restaurant['phone'] = descriptions.css('div.blEntry.phone').css('span.detail').xpath('text()').extract_first()
+        restaurant['address']  = descriptions.css('div.blEntry.address').css('span.detail').css('span.street-address').xpath('text()').extract_first()
+        
+        photos = response.css('div.photo_mosaic_and_all_photos_banner').css('div.prw_common_basic_image').css('img.basicImg').xpath('@src').extract()
+        if len(photos) > 5:
+            restaurant['images'] = photos[:5]
+        else:
+            restaurant['images'] = photos
 
-        big_details = response.css('div#RESTAURANT_DETAILS').css('div.details_tab')
-        rows = big_details.css('div.table_section').css('div.row')
-        for row in rows:
-            title = row.css('div.title').xpath('text()').extract_first()
-            if title != None and title.strip() == 'Meals':
-                meals = row.css('div.content').xpath('text()').extract_first()
-                if meals != None:
-                    restaurant['meals'] = meals.strip().split(', ')
+        details = response.css('div#taplc_detail_overview_cards_0')
+        for category_title in details.xpath("//div[contains(@class,'categoryTitle')]"):
+            title = category_title.xpath('text()').extract_first().strip()
+            tags = category_title.xpath('following-sibling::div[1]/text()').extract_first().strip()
+            if  title == 'CUISINES':
+                restaurant['cusines'] = tags.split(', ')
+            elif title == 'MEALS':
+                restaurant['meals'] = tags.split(', ')
+            elif title == 'PRICE RANGE':
+                prices = tags.replace('HK$', '').replace(' ' , '').split('-')
+                try:
+                    prices = [int(price) for price in prices]
+                except:
+                    continue
+                if len(prices) == 2:
+                    restaurant['price_range'] = {
+                        'min': prices[0],
+                        'max': prices[1]
+                    }
+                else:
+                    if prices[0] == 0:
+                        continue
+                    restaurant['price_average'] = prices[0]
+        try:
+            restaurant['rating'] = float(details.xpath("//span[contains(@class,'__overallRating')]/text()").extract_first().strip())
+            restaurant['ranking'] = int(details.xpath("//div[contains(@class,'__ranking')]").css('b > span').xpath('text()').extract_first().replace('#' , ''))
+        except:
+            pass
 
-        additional_info = big_details.css('div.additional_info').css('ul.detailsContent').xpath('li/text()').extract()
-        # print(additional_info)
-        for info in additional_info:
-            if info.strip().startswith("Neighbourhood: "):
-                restaurant['district'] = info.split(':')[1].strip()
-        # print(restaurant)
+        neighborhood = details.xpath("//span[contains(@class, '__nearbyText')]").css('div')
+        restaurant['district'] = neighborhood[0].xpath('text()').extract_first()
+        restaurant['close_to'] = neighborhood[1].xpath('text()').extract_first().replace('from ', '')
+
         return restaurant
-
-    
-
-
+        
